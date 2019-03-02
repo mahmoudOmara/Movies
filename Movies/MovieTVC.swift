4214d94b5c38aa4b08ce4f1ce186ec0cffd064fa
Mahmoud <mahmoud.omara@goodsmartegypt.com>
2019-03-02 19:02:40 +0200

    design

---
+++ Movies/MovieTVC.swift
@@ -0,0 +1,21 @@
+//
+//  MovieTVC.swift
+//  Movies
+//
+//  Created by Mahmoud Omara on 3/2/19.
+//  Copyright © 2019 Mahmoud Omara. All rights reserved.
+//
+
+import UIKit
+
+class MovieTVC: UITableViewCell {
+    
+    @IBOutlet weak var tittleL: UILabel!
+    @IBOutlet weak var posterIV: UIImageView!
+    @IBOutlet weak var overviewL: UILabel!
+    @IBOutlet weak var productionYearL: UILabel!
+    @IBOutlet weak var rateL: UILabel!
+    
+    
+    
+}
